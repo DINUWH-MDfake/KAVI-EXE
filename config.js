7b9d9c7a5bf852ecbc0fadc36f26dd20e327f8b7
DINUWH-MDfake <dinuwahmd@gmail.com>
2024-12-20 14:04:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,10 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "lp4RhQSB#nFZU6UQ5yqx3h6_iCkgcL-B1SfnayWQ2kJMEDHWbshk",
+SESSION_ID: process.env.SESSION_ID || "howWWZ6K#BcCHRdDPZFirWHI6wdSEO7igwVpxWoWry7oWvLL0I9Q",
 ALIVE_REACT: process.env.ALIVE_REACT || "👋",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://pomf2.lain.la/f/pkuujaup.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "KAVI EXE_V1_Connected ☑️_📂𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 KAVI_EXE📘☀",
+ALIVE_MSG: process.env.ALIVE_MSG || "DINU EXE_V1_Connected ☑️_📂𝙳𝙴𝙿𝙻𝚈 𝙱𝚈 DINU_EXE📘☀",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 ALWAYS_ONLINE : process.env.ALWAYS_ONLINE || "false",
